0500303e8e140c05cb3fdbaf6c9aadb8656e85e4
Mira Makadia <patelmirak@gmail.com>
2021-12-02 11:55:19 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const { application } = require('express');
+
+
+//Initialize app
+const app =express();
+
+//configure settings
+require('dotenv').config();
+
+//Database
+const{DATABASE_URL,  PORT} = process.env;
+
+//Connection to MongoDB
+mongoose.connect(DATABASE_URL);
+
+//Success/Error
+const db = mongoose.connection;
+db.on('connected', () => console.log('Connected to MongoDB'));
+db.on('error', (err) => console.log('MongoDB Error: ' + err.message));
+db.on('disconnected', () => console.log('MongoDB disconnected'));
+
+//Middleware
+//for public folder
+app.use(express.static('public'));
+//for req.body
+app.use(express.urlencoded({extended:false}));
+//method-override
+app.use(methodOverride('_method'));
+//https logger
+app.use(morgan('dev'));
+
+//Route
+app.get('/', (req, res) => {
+    res.render('home.ejs')
+});
+
+//Listener
+app.listen(PORT, () => {
+    console.log('Express is listening on port: ' + PORT)
+});
