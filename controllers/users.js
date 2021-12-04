92c1ad288c541581750b7eb133569a021218083e
Mira Makadia <patelmirak@gmail.com>
2021-12-04 13:20:29 -0500

    login route working

--- controllers/users.js
+++ controllers/users.js
@@ -17,24 +17,117 @@ usersRouter.get('/signup', (req, res) => {
 });
 
 usersRouter.post('/signup', (req, res) => {
-            const hash = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(SALT_ROUNDS));
-            const user = {
-                email: req.body.email,
-                password: hash
-            }
-            if (req.body.selection === 'driver') {
-                Driver.create(user, (error, driver) => {
-                    req.session.user = driver._id;
-                    res.redirect('/')
-                })
+    const hash = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(SALT_ROUNDS));
+    const user = {
+        email: req.body.email,
+        password: hash
+    }
+    if (req.body.selection === 'driver') {
+        Driver.create(user, (error, driver) => {
+            req.session.user = driver._id;
+            res.redirect('/')
+        })
+    } else {
+        Parent.create(user, (error, parent) => {
+            req.session.user = parent._id;
+            res.redirect('/')
+
+        })
+
+    };
+});
+usersRouter.get('/login', (req, res) => {
+    res.render('login.ejs', {
+        err: ''
+    });
+});
+
+usersRouter.post('/login', (req, res) => {
+    //find driver in database
+
+    //TODO: code needs to be refactored
+    Driver.findOne({
+        email: req.body.email
+    }, (err, driver) => {
+        if (driver) {
+            if (bcrypt.compareSync(req.body.password, driver.password)) {
+                req.session.user = driver._id;
+                res.redirect('/user/driversDashboard')
             } else {
-                Parent.create(user, (error, parent) => {
+                return res.render('login.ejs', {
+                    err: 'invalid creds'
+                });
+            }
+
+        } else {
+            // find parent in database
+            Parent.findOne({
+                email: req.body.email
+            }, (error, parent) => {
+                if (parent) {
+                    if (bcrypt.compareSync(req.body.password, parent.password)) {
                         req.session.user = parent._id;
-                        res.redirect('/')
+                        res.redirect('/user/parentsDashboard')
+                    } else {
+                        return res.render('login.ejs', {
+                            err: 'invalid creds'
+                        });
+                    }
+                } else {
+                    res.render('login.ejs', {
+                        err: 'invalid creds'
+                    });
+                }
 
-                })
 
-                };
+            })
+        }
+
+
+
+
+
+        //  find parent in database
+        /*Parent.findOne({
+            email: req.body.email
+        }, (error, parent) => {
+            console.log('err:', error)
+            console.log('parent:', parent)
+            if (!parent) return res.render('login.ejs', {
+                err: 'invalid creds'
             });
+            if (!bcrypt.compareSync(req.body.password, parent.password)) {
+                return res.render('login.ejs', {
+                    err: 'invalid creds'
+                });
+            }
+            req.session.user = parent._id
+            res.redirect('/user/parentsDashboard')
+        })*/
 
-            module.exports = usersRouter;
+    });
+});
+
+
+usersRouter.get('/driversDashboard', (req, res) => {
+    if (!req.session.user) return res.redirect('/user/login');
+    Driver.findById(req.session.user, (err, driver) => {
+        res.render('driversprofile.ejs', {
+            driver
+        });
+    });
+});
+
+usersRouter.get('/parentsDashboard', (req, res) => {
+    if (!req.session.user) return res.redirect('/user/login');
+    Parent.findById(req.session.user, (err, parent) => {
+        res.render('searchdriver.ejs', {
+            parent
+        });
+    });
+})
+
+
+
+
+module.exports = usersRouter;
