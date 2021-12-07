69084e794075d6c61de648f3b0c38b478fdc141c
Mira Makadia <patelmirak@gmail.com>
2021-12-06 20:25:49 -0500

    modifications on search function

--- public/js/search.js
+++ public/js/search.js
@@ -5,20 +5,34 @@ const ulElement = document.querySelector('ul');
 buttonElement.addEventListener('click', handleClick);
 inputElement.addEventListener('focus', handleReset);
 
-async function handleClick(){
+async function handleClick() {
     const searchTerm = inputElement.value;
+    if (!searchTerm) return alert('Sorry No Search Term Was Provided');
     const response = await fetch('/drivers/search?term=' + searchTerm);
     const data = await response.json();
-    
-    const list = data.results.map(driver => (
-        `<li style ="text-transform: capitalize;">
-            <a href="/drivers/${driver._id}">${driver.name}
-            </a>
-            </li>`
-    )).join('');
-    ulElement.innerHTML = list;
+    render();
+
+    function render() {
+        if (data.results.length === 0) {
+            ulElement.innerHTML = '<li> No Results</li>'
+        } else {
+            const list = data.results.map(driver => (
+                `<li style ="text-transform: capitalize;">
+                <a href="/drivers/${driver._id}">
+                ${driver.name}
+                </a>
+                </li>`
+            )).join('');
+            ulElement.innerHTML = list;
+
+        }
+        inputElement.value = "";
+
+    }
+
 }
 
+
 function handleReset() {
-    inputElement.innerHTML = "";
+    ulElement.innerHTML = "";
 }
